f0efc9d0ac43576f400c48970a5c1352844d2986
Marina Melaré <melare.nina@gmail.com>
2020-04-22 20:13:55 -0300

    Outputing elements - Binding to Custom Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: 'server', name: 'TestServer', content: 'Just a test!'}];
+  
+  // Every time we use {}, it means that is a JavaScript data
+  onServerAdded(serverData: {serverName: string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  } 
 }
